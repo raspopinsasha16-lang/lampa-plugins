484feb3f3818371dfab75cd636bcb45c80eb716c
raspopinsasha16-lang <raspopinsasha16@gmail.com>
2026-07-01 11:14:16 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,8 +1,8 @@
 /**
  * {
  * "name": "Хуямба",
- * "version": "1.1.0",
- * "description": "Huyamba Web Site",
+ * "version": "1.3.0",
+ * "description": "Открыть сайт можно прямо внутри этой карточки!",
  * "plugin": "huyamba_plugin"
  * }
  */
@@ -11,25 +11,13 @@
     'use strict';
 
     function startHuyamba() {
-        if (!window.Lampa || !window.Lampa.Menu) return;
+        if (!window.Lampa || !window.Lampa.Component) return;
 
-        var menuitem = {
-            id: 'huyamba_site',
-            title: 'Huyamba Name',
-            icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>',
-            name: 'Huyamba Name'
-        };
-
-        window.Lampa.Menu.add(menuitem);
-
+        // 1. Регистрируем экран с сайтом
         window.Lampa.Component.add('huyamba_component', function (object) {
-            var comp = this;
             var html;
-
             this.create = function () {
-                // Используем абсолютно безопасный способ создания разметки, совместимый со всеми версиями
                 html = window.Lampa.Template.get('activity_unset', {});
-                
                 var iframe = document.createElement('iframe');
                 iframe.src = 'https://ru.huyamba.name';
                 iframe.style.width = '100%';
@@ -37,42 +25,51 @@
                 iframe.style.border = 'none';
                 iframe.style.background = '#141414';
 
-                // Напрямую добавляем фрейм внутрь контейнера Lampa
                 if (html && html.get) {
                     html.get(0).appendChild(iframe);
                 } else if (html && html.append) {
                     html.append(iframe);
                 }
             };
-
-            this.render = function () {
-                return html;
-            };
-
-            this.destroy = function () {
-                if (html && html.remove) html.remove();
-            };
+            this.render = function () { return html; };
+            this.destroy = function () { if (html && html.remove) html.remove(); };
         });
 
-        window.Lampa.Listener.follow('menu', function (e) {
-            if (e.type === 'click' && e.item.id === 'huyamba_site') {
-                window.Lampa.Activity.push({
-                    title: 'Huyamba Name',
-                    component: 'huyamba_component',
-                    page: 1
-                });
-            }
-        });
+        // 2. Ловим открытие карточки нашего плагина
+        if (window.Lampa.Listener) {
+            window.Lampa.Listener.follow('activity', function (e) {
+                // Если открылась карточка плагина Хуямба
+                if (e.type === 'open' && e.component === 'plugins' && e.object && e.object.plugin === 'huyamba_plugin') {
+                    setTimeout(function () {
+                        var body = window.$('.extensions__item-info'); 
+                        if (!body.length || body.find('.btn-huyamba-start').length) return;
+
+                        // Создаем кнопку запуска
+                        var btn = window.$('<div class="extensions__item-btn btn-huyamba-start selector" style="background:#fff; color:#000; padding:10px 20px; margin-top:15px; text-align:center; border-radius:4px; font-weight:bold; cursor:pointer;">ЗАПУСТИТЬ САЙТ</div>');
+                        
+                        btn.on('hover:enter', function () {
+                            window.Lampa.Activity.push({
+                                title: 'Huyamba Name',
+                                component: 'huyamba_component',
+                                page: 1
+                            });
+                        });
+
+                        body.append(btn);
+                        
+                        if (window.Lampa.Controller && window.Lampa.Controller.refresh) {
+                            window.Lampa.Controller.refresh();
+                        }
+                    }, 150);
+                }
+            });
+        }
     }
 
-    if (window.Lampa && window.Lampa.Menu) {
-        startHuyamba();
-    } else {
-        var interval = setInterval(function () {
-            if (window.Lampa && window.Lampa.Menu) {
-                clearInterval(interval);
-                startHuyamba();
-            }
-        }, 300);
-    }
+    var interval = setInterval(function () {
+        if (window.Lampa && window.Lampa.Component) {
+            clearInterval(interval);
+            startHuyamba();
+        }
+    }, 300);
 })();
